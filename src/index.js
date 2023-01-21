84c6392212928ee4d3f79b18bf30ae746dc1ad22
Vital Hoza <43417456+Mechneiy@users.noreply.github.com>
2023-01-22 01:13:16 +0300

    feat: change file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let string = str.split("");
+  for (let i = 0; i < str.length; i++) {
+    bracketsConfig.map((index) => {
+      if (index[1] === string[i] && index[0] === string[i - 1]) {
+        string.splice(i - 1, 2);
+        i = 0;
+      }
+    });
+  }
+  return !string.length;
+};
